e26a9d8cd698341f8f985f1d77e97eb4959eb1f6
vuso2021 <ofa.faletau@outlook.com>
2021-03-03 21:32:44 +1300

    Update Section 1 and Section 2

--- jofa/src/App.js
+++ jofa/src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 // Header container
 function TopHeader() {
   return (
-    
+
     <div className="topNav">
 
       <div className="topLogo">
@@ -18,27 +18,44 @@ function TopHeader() {
       </div>
 
       <div className="topNavLinks">
-        <p>HOME </p>
-        <p>FEATURES</p>
-        <p>TEACHERS</p>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#" className="a1">HOME </a>
+            </li>
+            <li>
+              <a href="#" className="a1">FEATURES </a>
+            </li>
+            <li>
+              <a href="#" className="a1">TEACHERS </a>
+            </li>
+          </ul>
+        </nav>
+
       </div>
 
       <div className="topNavUser">
-
-        <div className="topFlagLang">
-
-          <p>LANG:
-            <img src={flagnz} alt="NZ Flag" />
-            <img src={flagmaori} alt="Maori Flag" />
-          </p>
-
-
-
-        </div>
         
-        <div className="topNavLogin">
-          <p> <i class="fa fa-user-circle" aria-hidden="true"></i>  REGISTER  |  LOGIN</p>
-        </div>
+              <div>
+                <div className="topFlagLang">
+                  <p>LANG: <img src={flagnz} alt="NZ Flag" /> <img src={flagmaori} alt="Maori Flag" />
+                  </p>
+                </div>
+              </div>
+            
+              <div >
+                <div className="topNavLogin">
+                <p>
+                  <a href="#" className="a1"><i class="fa fa-user-circle" aria-hidden="true"> </i>
+               REGISTER | LOGIN </a>
+                </p>
+                </div>
+              </div>
+          
+
+
+
 
       </div>
 
@@ -50,23 +67,23 @@ function TopHeader() {
 
 
 //Section 1
-function Section1(){
+function Section1() {
   return (
     <div className="sec1">
-      
+
       <div className="sec11">
         <h1> Prepare young minds for a better future.</h1>
 
-        <p className="psec1"> Let us help you advance students in Digital Technologies 
-          and other learning areas with our project-based learning programme.
+        <p className="psec1"> Let us help you advance students in Digital Technologies
+        and other learning areas with our project-based learning programme.
         </p>
 
         <button>LEARN MORE</button>
-        
+
         <button>SIGN UP</button>
-        
-        <p className="psec11">*Basic subscription includes the first 15 project 
-          free of charge.
+
+        <p className="psec11">*Basic subscription includes the first 15 project
+        free of charge.
         </p>
 
       </div>
@@ -76,6 +93,33 @@ function Section1(){
   );
 }
 
+
+//Section 2
+function Section2() {
+  return (
+    <div className="sec21">
+      <h2> What we offer </h2>
+      <p className="psec1"> 
+      The Creative Problem Solving programme is series of digital creation projects 
+      aimed to encourage self-motivation and student agency, designed by New Zealand's 
+      leading IT industry experts and schools.
+     <br></br>
+     </p>
+
+     <h3> What will students create? </h3>
+      <p className="psec1"> 
+      The Creative Problem Solving programme is series of digital creation projects 
+      aimed to encourage self-motivation and student agency, designed by New Zealand's 
+      leading IT industry experts and schools.
+     <br></br>
+     </p>
+
+      <div className="sec22">
+
+      </div>
+    </div>
+  );
+}
 
 
 
@@ -92,7 +136,8 @@ function App() {
   return (
     <div>
       <TopHeader />
-      <Section1/>
+      <Section1 />
+      <Section2 />
 
       <header className="App-header">
 
