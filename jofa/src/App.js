0c3c7505141f99201d7e6b5f0a1d2409bc6bcf7f
vuso2021 <ofa.faletau@outlook.com>
2021-03-03 19:39:04 +1300

    Section1 Update

--- jofa/src/App.js
+++ jofa/src/App.js
@@ -54,11 +54,38 @@ function Section1(){
   return (
     <div className="sec1">
       
-        <img src={baloon} className="baloonk"/>
-      
+      <div className="sec11">
+        <h1> Prepare young minds for a better future.</h1>
+
+        <p className="psec1"> Let us help you advance students in Digital Technologies 
+          and other learning areas with our project-based learning programme.
+        </p>
+
+        <button>LEARN MORE</button>
+        
+        <button>SIGN UP</button>
+        
+        <p className="psec11">*Basic subscription includes the first 15 project 
+          free of charge.
+        </p>
+
+      </div>
+
+      <div className="sec12"></div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 function App() {
