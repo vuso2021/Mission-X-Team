1ac2b8ba95fa14059b9a77ba7948ed12b1e229c6
vuso2021 <ofa.faletau@outlook.com>
2021-03-03 16:54:26 +1300

    update

--- jofa/src/App.js
+++ jofa/src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
+import star from './logo2.jpg';
+import flagnz from './img/nz.jpg';
+import flagmaori from './img/maori.jpg';
+import React from 'react';
 import './App.css';
+
+function Naav() {
+  return (
+    <div className="topNav">
+      
+
+      <div className="topLogo">
+      <img src={star} className="star" alt="starlogo" />
+      </div>
+
+      <div className="topNavLinks">
+          <p>HOME </p>
+          <p>FEATURES</p>
+          <p>TEACHERS</p>
+      </div>
+
+      <div className="topNavUser">
+        <div className='topNavLang'>
+          <p className="topFlagLang">LANG: 
+          <img src={flagnz} className="nzflag" alt="NZ Flag" />
+          <img src={flagmaori} className="maoriflag" alt="Maori Flag" />
+          </p>
+        </div>
+        
+        <p>REGISTER</p>
+        <p> | </p>
+        <p>LOGIN</p>
+
+      </div>
+
+    </div>
+
+
+  );
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Naav />
+      
       <header className="App-header">
+     
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
+
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
