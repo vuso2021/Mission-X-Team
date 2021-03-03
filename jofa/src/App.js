0596942b3099c63388ad0e4834f573d398291cf4
vuso2021 <ofa.faletau@outlook.com>
2021-03-03 19:06:22 +1300

    adding section1

--- jofa/src/App.js
+++ jofa/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import star from './logo2.jpg';
 import flagnz from './img/nz.jpg';
 import flagmaori from './img/maori.jpg';
+import baloon from './img/baloonkid.jpg';
+
 import React from 'react';
 import './App.css';
 
@@ -47,13 +49,23 @@ function TopHeader() {
 
 
 
-//
+//Section 1
+function Section1(){
+  return (
+    <div className="sec1">
+      
+        <img src={baloon} className="baloonk"/>
+      
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <TopHeader />
+      <Section1/>
 
       <header className="App-header">
 
